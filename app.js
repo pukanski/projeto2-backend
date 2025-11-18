b684f71417743f2db87e1441337d8357d25db8f5
Gustavo Pukanski <pukanski@hotmail.com>
2025-11-18 20:15:41 -0300

    backend final

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 const port = 8081; 
 const db = require('./models');
@@ -7,6 +9,7 @@ const auth = require('./middleware/auth');
 
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/', require('./routes/routeAuth'));
